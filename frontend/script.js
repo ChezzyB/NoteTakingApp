a1d2e48b57aef54574b244bafd8e96a6bcca24f0
Chesney Brooke <ChesneyBrooke@gmail.com>
2025-02-23 14:47:29 -0800

    full backend tested with basic test frontend and working for all CRUD operations

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 // Program's purpose is to allow the user to make notes and save them in a persistent state
-const apiUrl = "http://localhost5000/api/notes"
+const apiUrl = "http://localhost:5000/api/notes/"
 
 async function getNotes(){
     const response = await fetch(apiUrl);
@@ -12,8 +12,8 @@ function renderNotes(notes) {
     noteList.innerHTML = ''; //Clear the list
 
     notes.forEach(note => {
-        // const li = document.createElement('li');
-        // li.classList.toggle('completed',note.completed);
+        const li = document.createElement('li');
+        //li.classList.toggle('completed',note.completed);
 
         // Create span for the title
         const span = document.createElement('span');
@@ -28,22 +28,22 @@ function renderNotes(notes) {
         li.appendChild(span);
         li.appendChild(deleteButton);
 
-        //Append the list item to the todo list
-        todoList.appendChild(li);
+        //Append the list item to the note list
+        noteList.appendChild(li);
     });
 }
 
 async function addNote(event) {
     event.preventDefault();
     const title = document.getElementById('title').value;
-    const description = document.getElementById('description').value;
+    const contents = document.getElementById('contents').value;
 
     try{
-        if (title && description) {
+        if (title && contents) {
             const response = await fetch(apiUrl, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json'},
-                body: JSON.stringify({title, description})
+                body: JSON.stringify({title, contents})
             });
             if (!response.ok){
                 const errorData = await response.json();
@@ -53,7 +53,7 @@ async function addNote(event) {
             }
             await response.json();
             title.value = '';
-            description.value = '';
+            contents.value = '';
             getNotes();
         }
     } catch (error) {
@@ -62,7 +62,7 @@ async function addNote(event) {
 }
 
 async function deleteNote(id) {
-    await fetch('${apiUrl}/${id}', {method : 'DELETE'});
+    await fetch(`${apiUrl}/${id}`, {method : 'DELETE'});
     getNotes();
 }
 
